5717944e214b2183c7fe1596b5bdcc66b6c248ad
fernandat} <trevinofernanda69@gmail.com>
2026-04-27 19:52:20 -0600

    Se agregan tres funciones básicas

---
+++ main.py
@@ -0,0 +1,9 @@
+from funciones import suma, resta, multiplicacion
+
+resultado_suma = suma(10, 5)
+resultado_resta = resta(10, 5)
+resultado_multi = multiplicacion(10, 5)
+
+print(f"Suma: {resultado_suma}")
+print(f"Resta: {resultado_resta}")
+print(f"Multiplicación: {resultado_multi}")
